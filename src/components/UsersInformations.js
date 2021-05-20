d2a916e746fda0eb4e75b8c5d99032d9e60617a7
Arichson <arichsonh@gmail.com>
2021-05-19 19:06:07 -0700

    pretty much done

--- src/components/UsersInformations.js
+++ src/components/UsersInformations.js
@@ -25,9 +25,6 @@ export default function UsersInformations ({userInfo}){
             console.log(err)
         }
     }
-
-
-
     const editUserInformation = async (e) => {
         // e.preventDefault();
 
@@ -61,22 +58,25 @@ export default function UsersInformations ({userInfo}){
     }, [userInfo])
     
     return(
-        <div>
+        <div className="m-5 border-black border-2 p-2 rounded">
         {information ?
             <div>
-                <button onClick={() => setEditInfo(!editInfo)}>{editInfo? "Cancel" : "Edit Information"}</button>
+                <div className="flex justtify-between w-full">
+                    <button className="w-20 bg-gray-500 border-2 border-black rounded mr-28" onClick={() => setEditInfo(!editInfo)}>{editInfo? "Cancel" : "Edit"}</button>
+                    {editInfo ? <button className="w-20 bg-gray-500 border-2 border-black rounded flex-inline justify-end" onClick={deleteUser}>Delete Account</button> : ""} 
+                </div>
                 {
                     editInfo? 
                     <div>
                         <form onSubmit={editUserInformation}>
-                            User Name: <input type="text" name="username" onChange={handleChange} defaultValue={userInfo.username} /> <br /> <br />
-                            First Name: <input type="text" name="firstName" onChange={handleChange} defaultValue={userInfo.firstName} /> <br /> <br />
-                            Last Name: <input type="text" name="lastName" onChange={handleChange} defaultValue={userInfo.lastName} /> <br /> <br />
-                            Birthdate: <input type="text" name="birthday" onChange={handleChange} defaultValue={userInfo.birthday} /> <br /> <br />
-                            About Me: <input type="text" name="bio" onChange={handleChange} defaultValue={userInfo.bio} /> <br /> <br />
-                            <input type="submit" />
+                            User Name: <input type="text" name="username" onChange={handleChange} defaultValue={userInfo.username} className="border-blue border-2 p-1 rounded" /> <br /> <br />
+                            First Name: <input type="text" name="firstName" onChange={handleChange} defaultValue={userInfo.firstName} className="border-blue border-2 p-1 rounded" /> <br /> <br />
+                            Last Name: <input type="text" name="lastName" onChange={handleChange} defaultValue={userInfo.lastName} className="border-blue border-2 p-1 rounded" /> <br /> <br />
+                            Birthdate: <input type="text" name="birthday" onChange={handleChange} defaultValue={userInfo.birthday} className="border-blue border-2 p-1 rounded" /> <br /> <br />
+                            <p>About Me:</p><textarea name="bio" onChange={handleChange} defaultValue={userInfo.bio} className="border-blue border-2 p-1 rounded w-2/5 h-40"/> <br /> <br />
+                            
+                            <input className="w-20 bg-gray-500 border-2 border-black rounded" type="submit" />
                         </form>
-                        <button onClick={deleteUser}>Delete Account</button>
                     </div>
                     :
                     <div>
@@ -84,7 +84,7 @@ export default function UsersInformations ({userInfo}){
                         <p>First Name: {userInfo.firstName}</p>
                         <p>Last Name: {userInfo.lastName}</p>
                         <p>Birthdate: {userInfo.birthday}</p>
-                        <p>About Me: {userInfo.bio}</p>
+                        <p className="border-black border-2 p-2 rounded">About Me: {userInfo.bio}</p>
                     </div>
                 }
 
